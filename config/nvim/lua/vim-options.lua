f8fa0b5b53ca4d41d917ea9bc160acf8ff1a46f0
Vittorio <vittorio.candiello@gmail.com>
2025-05-29 10:30:59 +0200

    remapping of []{}

--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -1,3 +1,24 @@
 vim.g.mapleader = " "
 vim.cmd(":set number")
 vim.cmd(":set relativenumber")
+
+vim.cmd(":nnoremap ò [")
+vim.cmd(":nnoremap à ]")
+vim.cmd(":nnoremap ç {")
+vim.cmd(":nnoremap ° }")
+
+vim.cmd(":inoremap ò [")
+vim.cmd(":inoremap à ]")
+vim.cmd(":inoremap ç {")
+vim.cmd(":inoremap ° }")
+
+vim.cmd(":vnoremap ò [")
+vim.cmd(":vnoremap à ]")
+vim.cmd(":vnoremap ç {")
+vim.cmd(":vnoremap ° }")
+
+vim.cmd(":cnoremap ò [")
+vim.cmd(":cnoremap à ]")
+vim.cmd(":cnoremap ç {")
+vim.cmd(":cnoremap ° }")
+
