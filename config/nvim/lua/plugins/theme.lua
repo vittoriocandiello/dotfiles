652c0bbe220349f60ce9e20df62974cdce336d7c
Vittorio <vittorio.candiello@gmail.com>
2025-06-16 08:35:52 +0200

    harpoon, minimal tmux and added theme for nvim

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,19 @@
 return {
 	"catppuccin/nvim",
+	--"vague2k/vague.nvim",
 	lazy = false,
-	priority = 1000 ,
+	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme "catppuccin"
-	end
+		--for catppuccin:
+		require("catppuccin").setup({
+			flavour = "mocha",
+			transparent_background = true,
+		})
+		vim.cmd.colorscheme("catppuccin")
+
+		--for vague:
+		--require("vague").setup({ transparent = true })
+		--vim.cmd("colorscheme vague")
+		--vim.cmd(":hi statusline guibg=NONE")
+	end,
 }
