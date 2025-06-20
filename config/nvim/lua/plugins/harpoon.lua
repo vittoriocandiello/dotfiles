675ee5bdd8289bba6f7d61db0efc8aea27d6a093
Vittorio <vittorio.candiello@gmail.com>
2025-06-20 09:51:32 +0200

    Deleted xremap

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -10,6 +10,6 @@ return {
         vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-[>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
     end
 }
--- config/xremap/readme.txt
+++
@@ -1,6 +0,0 @@
-make the executable run this command: 
-then create the link with:
-
-chmod +x ~/.dotfiles/config/xremap/remapkeys
-
-ln -s ~/.dotfiles/config/xremap/remapkeys ~/.local/bin/remapkeys
