8ed84c19bd6bd36a28f74c7bca95d6373199cd7d
Vittorio <vittorio.candiello@gmail.com>
2025-09-03 17:15:36 +0200

    swap files off

--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -7,5 +7,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
+vim.opt.swapfile = false
+
 
 vim.keymap.set('n', '<leader>cr', ':!g++ main.cpp -o exec && ./exec<CR>', {})
